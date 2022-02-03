1757dcb791a165028b59b27af1263dea14d816ea
Kidus Abrham <study.kidusabrham@gmail>
2022-02-03 11:37:20 -0800

    Day-2 | Signup page completed

--- api/routes/views.js
+++ api/routes/views.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const views = express.Router();
 
-views.get("/", (req, res) => {
+views.get("/login", (req, res) => {
     res.render("login");
+})
+
+views.get("/signup", (req, res) => {
+    res.render("signup");
 })
 
 module.exports = views;
